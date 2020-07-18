02a1747fe1518d831b54b99a294b76a2d46e0a66
patternizer <patternizer@gmail.com>
2020-07-18 22:51:02 +0100

    Add block to loop over 50x50 matrices and perform sensitivity analysis

--- E2.py
+++ E2.py
@@ -1,5 +1,7 @@
 import numpy as np
 from functools import reduce
+from numpy import linalg as LA
+import matplotlib.pyplot as plt        
 
 
 # Calculate the norm square of the jth element of the ith eigenvector using the alternate algorithm
@@ -139,21 +141,60 @@ def compare(H, mode=0):
 
 
 if __name__ == "__main__":
-    # Generate a random Hermitian matrix
-    # Dimension of the matrix
-    n = 8
-    H = np.random.rand(n, n) + 1j * np.random.rand(n, n)
-    H = 0.5 * (H + H.T)
+    
+    # FPE as a function of matrix size or not
+    sensitivity_analysis = True
 
-    # Print the second element of the first eigenvector as calculated in both methods
-    print("\n--- using the algorithm ---")
-    basic = eig_sq(H)
-    print("basic implementation: ", basic[0, 1])
-    perf = eig_sq_perf(H)
-    print("performant implementation: ", perf[0, 1])
-    vect = eig_sq_vect(H)
-    print("vectorized implementation implementation: ", vect[0, 1])
+    if not sensitivity_analysis:
+        
+        # Generate a random Hermitian matrix
+        # Dimension of the matrix
+            
+        n = 8
+        H = np.random.rand(n, n) + 1j * np.random.rand(n, n)
+        H = 0.5 * (H + H.T)
 
-    print("\n--- using numpy ---")
-    nump = eig_sq_np(H)
-    print(nump[0, 1])
+        # Print the second element of the first eigenvector as calculated in both methods
+        print("\n--- using the algorithm ---")
+        basic = eig_sq(H)
+        print("basic implementation: ", basic[0, 1])
+        perf = eig_sq_perf(H)
+        print("performant implementation: ", perf[0, 1])
+        vect = eig_sq_vect(H)
+        print("vectorized implementation implementation: ", vect[0, 1])
+        
+        print("\n--- using numpy ---")
+        nump = eig_sq_np(H)
+        print(nump[0, 1])
+    
+    else:
+        nmin = 2
+        nmax = 50
+        fpe_basic_array = np.zeros([nmax-nmin+1])
+        fpe_perf_array = np.zeros([nmax-nmin+1])
+        fpe_vect_array = np.zeros([nmax-nmin+1])
+        for i in range(nmin,nmax+1):    
+            n = i
+            H = np.random.rand(n, n) + 1j * np.random.rand(n, n)
+            H = 0.5 * (H + H.T)
+            basic = eig_sq(H)
+            perf = eig_sq_perf(H)
+            vect = eig_sq_vect(H)
+            nump = eig_sq_np(H)
+            fpe_basic = 100.*(LA.norm(basic)-LA.norm(nump))/LA.norm(basic)
+            fpe_perf = 100.*(LA.norm(perf)-LA.norm(nump))/LA.norm(perf)
+            fpe_vect = 100.*(LA.norm(vect)-LA.norm(nump))/LA.norm(vect)
+            fpe_basic_array[i-nmin] = fpe_basic
+            fpe_perf_array[i-nmin] = fpe_perf
+            fpe_vect_array[i-nmin] = fpe_vect
+
+        fig,ax = plt.subplots(figsize=(15,10))
+        plt.scatter(np.arange(nmin, nmax+1), fpe_basic_array, label='f(basic-numpy)', marker='o', color='black')
+        plt.scatter(np.arange(nmin, nmax+1), fpe_perf_array, label='f(perf-numpy)', marker='.', color='red')
+        plt.scatter(np.arange(nmin, nmax+1), fpe_vect_array, label='f(vect-numpy)', marker='o', color='grey')
+        plt.legend()
+        plt.xlabel('Matrix size, n')
+        plt.ylabel('FPE, %')
+        plt.title('Sensitivity analysis')
+        plt.savefig('sensitivity_analysis.png')
+        
